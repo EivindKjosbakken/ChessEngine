68c197b63469e321281972997586305911256a0e
Stefan Kuhn <stefan.kuhn.zurich@gmail.com>
2024-07-02 21:14:53 +0200

    remove duplicate method 'encodeBoard'

--- PythonVersion/libs/HelperFunctions.py
+++ PythonVersion/libs/HelperFunctions.py
@@ -201,31 +201,31 @@ def encodeMove(move: str, board) -> int:
     return action
 
 def encodeBoard(board: chess.Board) -> np.array:
- """Converts a board to numpy array representation."""
+    """Converts a board to numpy array representation."""
 
- array = np.zeros((8, 8, 14), dtype=int)
+    array = np.zeros((8, 8, 14), dtype=int)
 
- for square, piece in board.piece_map().items():
-  rank, file = chess.square_rank(square), chess.square_file(square)
-  piece_type, color = piece.piece_type, piece.color
- 
-  # The first six planes encode the pieces of the active player, 
-  # the following six those of the active player's opponent. Since
-  # this class always stores boards oriented towards the white player,
-  # White is considered to be the active player here.
-  offset = 0 if color == chess.WHITE else 6
-  
-  # Chess enumerates piece types beginning with one, which you have
-  # to account for
-  idx = piece_type - 1
- 
-  array[rank, file, idx + offset] = 1
+    for square, piece in board.piece_map().items():
+        rank, file = chess.square_rank(square), chess.square_file(square)
+        piece_type, color = piece.piece_type, piece.color
 
- # Repetition counters
- array[:, :, 12] = board.is_repetition(2)
- array[:, :, 13] = board.is_repetition(3)
+        # The first six planes encode the pieces of the active player,
+        # the following six those of the active player's opponent. Since
+        # this class always stores boards oriented towards the white player,
+        # White is considered to be the active player here.
+        offset = 0 if color == chess.WHITE else 6
 
- return array
+        # Chess enumerates piece types beginning with one, which we have
+        # to account for
+        idx = piece_type - 1
+
+        array[rank, file, idx + offset] = 1
+
+    # Repetition counters
+    array[:, :, 12] = board.is_repetition(2)
+    array[:, :, 13] = board.is_repetition(3)
+
+    return array
 
 def encodeBoardFromFen(fen: str) -> np.array:
  board = chess.Board(fen)
@@ -463,30 +463,3 @@ def decodeMove(action: int, board) -> chess.Move:
                 move.promotion = chess.QUEEN
 
         return move
-
-def encodeBoard(board: chess.Board) -> np.array:
-	"""Converts a board to numpy array representation."""
-
-	array = np.zeros((8, 8, 14), dtype=int)
-
-	for square, piece in board.piece_map().items():
-		rank, file = chess.square_rank(square), chess.square_file(square)
-		piece_type, color = piece.piece_type, piece.color
-	
-		# The first six planes encode the pieces of the active player, 
-		# the following six those of the active player's opponent. Since
-		# this class always stores boards oriented towards the white player,
-		# White is considered to be the active player here.
-		offset = 0 if color == chess.WHITE else 6
-		
-		# Chess enumerates piece types beginning with one, which we have
-		# to account for
-		idx = piece_type - 1
-	
-		array[rank, file, idx + offset] = 1
-
-	# Repetition counters
-	array[:, :, 12] = board.is_repetition(2)
-	array[:, :, 13] = board.is_repetition(3)
-
-	return array
